7f70a219fbc1f905cc9118caf817921f8989883d
corwin <the.awesome.frost@gmail.com>
2020-10-20 14:42:46 +0200

    the project
    handle this with great care or the war drums will come for us all

---
+++ Memory/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Memory.Properties;
+using System.IO;
+using System.Media;
+using System.Threading;
+using Memory.Classes;
+
+namespace Memory
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        /// <summary>
+        /// Loads highscores
+        /// Makes sure theme works
+        /// Toggles the background music on or off depending on the settings
+        /// Loads WelkomPage.xaml
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // initialize the beginner frame with the "WelkomPage" view//
+            var welcomeUri = new Uri("WelkomPage.xaml", UriKind.Relative);
+            frmMainContent.Source = welcomeUri;
+        }
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void mainWindow_Closed(object sender, EventArgs e)
+        {
+            sp.Stop();
+            Application.Current.Shutdown();
+        }
+    }
+}
---
+++ Memory/PausePage.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Memory
+{
+    /// <summary>
+    /// Interaction logic for PausePage.xaml
+    /// </summary>
+    public partial class PausePage : Page
+    {
+        public PausePage()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Navigates to WelkomPage
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Instellingenbtn_Click(object sender, RoutedEventArgs e)
+        {
+            // through the course of clicking the button the navigation system switches the current frame uri to the new one//
+            NavigationService.Navigate(new InstellingenPage());
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Startoverbtn_Click(object sender, RoutedEventArgs e)
+        {
+            // through the course of clicking the button the navigation system switches the current frame uri to the new one//
+            NavigationService.Navigate(new MemoryPage());
+        }
+        /// <summary>
+        /// Navigates to MemoryPage after resuming game
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Hervatbtn_Click(object sender, RoutedEventArgs e)
+        {
+            // through the course of clicking the button the navigation system switches the current frame uri to the new one//
+            NavigationService.Navigate(new MemoryPage());
+        }
+        /// <summary>
+        /// Saves game and navigates WelkomPage
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Opslaanbtn_Click(object sender, RoutedEventArgs e)
+        {
+            // through the course of clicking the button the navigation system switches the current frame uri to the new one//
+            NavigationService.Navigate(new WelkomPage());
+        }
+        /// <summary>
+        /// Deletes your current game and navigates to WelkomPage
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Quitbtn_Click(object sender, RoutedEventArgs e)
+        {
+            // through the course of clicking the button the navigation system switches the current frame uri to the new one//
+            NavigationService.Navigate(new WelkomPage());
+        }
+    }
+}
